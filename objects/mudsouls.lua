b6b57086d2867c9d971620c82d1084c7ff481c63
Davenge <korisd@gmail.com>
2015-10-15 17:43:45 -0400

    working on some mudsoul methods

--- objects/mudsouls.lua
+++ objects/mudsouls.lua
@@ -11,11 +11,30 @@ function S:new( client )
    s.client = client
    s.entity = {}
    s.entity_index = 0
-
+   s.previous_index = nil
    -- add it to the global module
    S.souls[#S.souls+1] = s
 
    return s
+end
+
+function S:addEntity( entity )
+   self.entity[#self.entity+1] = entity
+   return #self.entity
+end
+
+function S:remEntity( entity )
+end
+
+function S:remEntityByIndex( index )
+end
+
+function S:setIndex( index )
+   if( self.entity_index[index] ) then
+      self.entity_index = index
+      return true
+   end
+   return false
 end
 
 return S
