9766f22e1e921d7a37cc7b2a595d099a53f15479
Davenge <korisd@gmail.com>
2015-10-15 19:40:54 -0400

    taking a break for now, need to reevaluate data serialization

--- objects/entity.lua
+++ objects/entity.lua
@@ -1,14 +1,16 @@
 local E = {}
 
-E.ACCOUNT = 1
-E.GAMEOBJ = 2
-E.NANNY   = 3
-
-E.types = { "account", "gameobj", "nanny" }
-
+E.ACCOUNT  = 1
+E.GAMEOBJ  = 2
+E.NANNY    = 3
+E.types    = { "account", "gameobj", "nanny" }
 E.entities = {}
 
-function E:new( path )
+for i, t in pairs( E.types ) do
+   E.entities[i] = {}
+end
+
+function E:new( type )
 end
 
 function E:load()
