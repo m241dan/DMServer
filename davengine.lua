d50c7c956df2c218f473f261ab6b6439322f3f7c
Davenge <korisd@gmail.com>
2015-10-17 13:32:59 -0400

    deleted mudsouls, going with more standard conventions

--- davengine.lua
+++ davengine.lua
@@ -11,6 +11,7 @@ global_events = {}
 function main()
    print( "DavEngine starting..." )
    print( "Setting up Server!" )
+
    -- setup the server on port 6500
    mudserver = assert( Server:new( 6500 ), "could not bind mud server" )
    -- "start" simply means it will accept new connections when :accept() is called
@@ -19,10 +20,9 @@ function main()
    -- setup our event for accepting new connections
    mudserver.acceptEvent = EventQueue.event:new( MudServerEvents.acceptNewConnections, EventQueue.second, { mudserver }, "Accepting new connections event." )
    EventQueue.insert( mudserver.acceptEvent )
-   global_events[#global_events+1] = mudserver.acceptEvent
 
    -- setup the polling for new input, processing new input, pushing new input
-   local poll_process_push = EventQueue.event:new( MudServerEvents.pollProcessPush, EventQueue.default_tick(), nil, "
+--   local poll_process_push = EventQueue.event:new( MudServerEvents.pollProcessPush, EventQueue.default_tick(), nil, "
    print( "Running the Event Queue!" )
    EventQueue.run()
    print( "DavEngine finished." )
--- objects/entity.lua
+++ objects/entity.lua
@@ -1,22 +1,8 @@
 local E = {}
 
-E.ACCOUNT  = 1
-E.GAMEOBJ  = 2
-E.NANNY    = 3
-E.types    = { "account", "gameobj", "nanny" }
 E.entities = {}
 
-for i, t in pairs( E.types ) do
-   E.entities[i] = {}
-end
-
-function E:new( type )
-end
-
-function E:load()
-end
-
-function E:instance()
+function E:new()
 end
 
 return E
