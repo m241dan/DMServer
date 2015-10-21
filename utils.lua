a2170e4b5cdf98a0327547ed629feea33b3a6799
Davenge <korisd@gmail.com>
2015-10-21 17:02:28 -0400

    made the first "serializer" for utils.save

--- utils.lua
+++ utils.lua
@@ -19,4 +19,31 @@ function table.getn( table )
    return count
 end
 
+local function serialize( data, indent_amount )
+   if( type( data ) == "string" ) then
+      return string.format( "%q", data ) 
+   elseif( type( data ) == "number" ) then
+      return tostring( data )
+   else
+      if( not data.serialize ) then
+         local tab_str, str = { "{" }, nil
+         indent_amount = indent_amount + 3
+         for k, v in pairs( data ) do
+            str = string.format( "%s[%s] = %s,", string.rep( " ", indent_amount ), serialize( k, indent_amount ), serialize( v, indent_amount ) )
+            tab_str[#tab_str+1] = str
+         end
+         tab_str[#tab_str+1] = string.format( "%s}", string.rep( " ", indent_amount - 3 ) )
+         return table.concat( tab_str, "\n" )
+      else
+         return data:serialize()
+      end
+   end
+end
+
+function utils.save( data, file )
+   file:write( "return " )
+   file:write( serialize( data, 0 ) )
+end
+
 return utils
+
