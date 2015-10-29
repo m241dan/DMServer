69070852bec437c6eec9086093ad89600e90ca30
Davenge <korisd@gmail.com>
2015-10-29 17:35:38 -0400

    forgot events have to return something

--- events/server.lua
+++ events/server.lua
@@ -3,6 +3,7 @@
 local M = {}
 
 M.accept_new = 1000
+M.ppp_interval = 100
 
 function M.acceptNewConnections( server )
    local nc, sm, s
@@ -19,6 +20,19 @@ function M.acceptNewConnections( server )
 end
 
 function M.pollProcessPush( server )
+   -- poll clients for new input
+   server:poll()
+   -- interpret client input using the associated statemanager's current state's interpreter
+   for index, client in ipairs( server.connections ) do
+      local sm = StateManager.managers_by_client[client]
+      for _, ln in ipairs( client.inbuf ) do
+         sm.states[sm.current].interpreter( ln )
+      end
+      client.inbuf = {}
+   end
+   -- push client outbufs to client
+   server:push()
+   return M.ppp_interval
 end
 
 return M
