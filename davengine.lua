67a946db46be2f5669b09d4aef807becbc6165f6
Davenge <korisd@gmail.com>
2015-10-29 17:35:55 -0400

    added the pollprocesspush event

--- davengine.lua
+++ davengine.lua
@@ -14,8 +14,11 @@ function main()
    mudserver:start()
 
    -- setup our event for accepting new connections
-   mudserver.acceptEvent = Event:new( MudServerEvents.acceptNewConnections, EventQueue.second, { mudserver }, "Accepting new connections event." )
-   EventQueue.insert( mudserver.acceptEvent )
+   mudserver.accept_event = Event:new( MudServerEvents.acceptNewConnections, EventQueue.second, { mudserver }, "Accepting new connections event." )
+   EventQueue.insert( mudserver.accept_event )
+
+   mudserver.poll_process_push = Event:new( MudServerEvents.pollProcessPush, 100, { mudserver }, "Poll. Process. Push." )
+   EventQueue.insert( mudserver.poll_process_push )
 
    -- setup the polling for new input, processing new input, pushing new input
 --   local poll_process_push = EventQueue.event:new( MudServerEvents.pollProcessPush, EventQueue.default_tick(), nil, "
