94bf18f6dff6f4401c334387d9ca3b340fb5caff
Davenge <korisd@gmail.com>
2016-01-04 22:24:49 -0500

    finished up the test of basics for datamanager

--- tests/datamanager_basic_test.lua
+++ tests/datamanager_basic_test.lua
@@ -20,39 +20,52 @@ end
 -- Add and Set data(set makes it the current data)
 -- Set up an interpreter using the function we wrote above: itp()
 -- Send it some stuff
-
 function main()
    local dm = DataManager:new()
    local data = { dt = "hi, I'm dt!" }
    dm:AASData( data )
    dm:setupInterp( data, itp )
    
-   dm:interpreter[data]( "Hi" )
-   dm:interpreter[data]( "Testing!" )
-   dm:interpreter[data]( "dttest" )
-   dm:interpreter[data]( nil )
+   dm:interp( "Hi" )
+   dm:interp( "Testing!" )
+   dm:interp( "dttest" )
+   dm:interp( nil )
 end
---[[
+
 function second_main()
-   local sm = StateManager:new()
-   local state = State:new( { dt = "hi, I'm dt!" }, behaviour, behaviour.getInterp() )
-   sm:addState( state )
-   state.interpreter( "Hi" )
-   state.interpreter( "Testing!" )
-   state.interpreter( "dttest" )
+   local dm = DataManager:new()
+   local d1 = { dt = "hi, I'm dt!" }
+   local d2 = { dt = "hi, I'm dt!" }
+   local d3 = { dt = "hi, I'm dt!" }
+   local d4 = { dt = "hi, I'm dt!" }
+   dm:AASData( d1 )
+   dm:setupInterp( d1, itp )
+   dm:addData( d2 )
+   dm:setupInterp( d2, itp )
+   dm:addData( d3 )
+   dm:setupInterp( d3, itp )
+   dm:addData( d4 )
+   dm:setupInterp( d4, itp )
+
+   dm:remData( d1 )
+   dm:interp( "Hi" )
+   dm:interp( "Testing!" )
+   dm:interp( "dttest" )
+   dm:setData_byData( d3 )
+   dm:interp( "Hi" )
+   dm:interp( "Testing!" )
+   dm:interp( "dttest" )
+   dm:interp( nil )
 end
---]]
 
 print( "Starting Test One\n" )
 main()
-collectgarbage()
-StateManager.dataDump()
-print( "The dump should read four 0s" )
+collectgarbage( "collect" )
+DataManager.dataDump()
+print( "The dump should read all 0s!" )
 
---[[
 print( "\nStarting Test Two\n" )
 second_main()
 collectgarbage()
-StateManager.dataDump()
-print( "The dump should read three 1s, 0 on client" )
---]]
+DataManager.dataDump()
+print( "The dump should read 1 all manager and 2 by data!" )
