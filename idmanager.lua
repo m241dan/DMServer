0897411a7e7884c59ef2f3caa5595ea8a75a8a75
Davenge <korisd@gmail.com>
2016-01-28 20:07:50 -0500

    added the idmanager.lua file, so far it is just a description

---
+++ idmanager.lua
@@ -0,0 +1,9 @@
+----------------------------------------------------
+-- ID Manager Library, written by Daniel R. Koris --
+----------------------------------------------------
+-- The ID Manager Library creates and manages ID  --
+-- tags for specific to types of data. It is      --
+-- designed to be fully compatible with the       --
+-- DMServer framework.                            --
+----------------------------------------------------
+
